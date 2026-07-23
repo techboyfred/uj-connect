dcbdb8f881bcdc2332144b6d0cf1da6a7f8177f3
Freddy <148700928+techboyfred@users.noreply.github.com>
2026-07-23 22:39:28 +0200

    Add files via upload

---
+++ Controllers/MarketLoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using System.ComponentModel.DataAnnotations;
+using UJConnect.Data;
+using UJConnect.Models;
+
+namespace UJConnect.Controllers
+{
+    public class MarketLoginController : Controller
+    {
+        private readonly UserDAO _userDAO;
+
+        public MarketLoginController(UserDAO userDAO)
+        {
+            _userDAO = userDAO;
+        }
+
+        //GET: /MarkertLogin/MarketLogin, this will be the empty login form
+        [HttpGet]
+        public IActionResult MarketLogin()
+        {
+            return View();
+        }
+
+        //POST: /MarketLogin/MarketLogin, this will handle the submitted form
+        [HttpPost]
+        public IActionResult MarketLogin(string username, string password)
+        {
+            User? user = _userDAO.LoginWithUsername(username, password);
+
+
+            if (user == null)
+            {
+                ViewData["ErrorMessage"] = "Invalid username or password.";
+                return View();
+            }
+
+            //store important data
+            HttpContext.Session.SetInt32("userID", user.UserID);
+            HttpContext.Session.SetString("username", user.Username);
+
+            return RedirectToAction("MarketHomeController");
+        }
+    }
+}
+
